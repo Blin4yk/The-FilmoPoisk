569576ef8ffbae8baf8d10a458145138b76a72f2
Никита Шелонин <nshelonin@mail.ru>
2025-12-04 20:55:58 +0300

    nginx

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 import contextlib
 from collections.abc import AsyncIterator
 
-import uvicorn
 from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
 
-from core.config import settings
 from api.v1 import films
+from core.config import settings
 from db import elastic, redis
 
 
