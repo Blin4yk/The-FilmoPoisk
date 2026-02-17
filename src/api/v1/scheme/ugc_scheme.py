200e1a029614f785b927aaed16e1d2c9f8b6403f
Никита Шелонин <nshelonin@mail.ru>
2026-02-17 22:22:34 +0300

    схема ugc

---
+++ src/api/v1/scheme/ugc_scheme.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class BookmarkIn(BaseModel):
+    film_id: str = Field(...)
+    note: str = Field(...)
+
+
+class BookmarkOut(BookmarkIn):
+    id: str
+    user_id: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class LikeIn(BaseModel):
+    film_id: str = Field(...)
+    value: int = Field(..., ge=-1, le=1, description="-1 дизлайк, 1 лайк")
+
+
+class LikeOut(LikeIn):
+    id: str
+    user_id: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class ReviewIn(BaseModel):
+    film_id: str = Field(...)
+    title: str = Field(..., min_length=3, max_length=100)
+    text: str = Field(..., min_length=10, max_length=5000)
+    rating: int = Field(..., ge=1, le=10)
+
+
+class ReviewUpdate(BaseModel):
+    title: str | None = Field(..., min_length=3, max_length=100)
+    text: str | None = Field(..., min_length=3, max_length=5000)
+    rating: int = Field(..., ge=1, le=10)
+
+
+class ReviewOut(ReviewIn):
+    id: str
+    user_id: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class DeleteResponse(BaseModel):
+    status: str = 'ok'
+    timestamp: datetime = Field(default_factory=lambda: datetime.now())
